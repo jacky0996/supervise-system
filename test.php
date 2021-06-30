1292e5c83068e8e6ae3b95fae05a11879c69666e
Shane Lin 林楨祥 <shanelin@giga.hgc.com.tw>
2021-06-30 13:49:11 +0800

    test

--- test.php
+++ test.php
@@ -10,9 +10,9 @@ $ch = curl_init();
 curl_setopt($ch , CURLOPT_URL , "https://supervise-system.herokuapp.com/test.php");
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $message);
 var_dump('123456');
 exit();
+curl_setopt($ch, CURLOPT_POSTFIELDS, $message);
 $result = curl_exec($ch);
 curl_close($ch);
 ?>
