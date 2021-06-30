44cf6b94415a50f1bb4203989a1d98315fdb23a9
Shane Lin 林楨祥 <shanelin@giga.hgc.com.tw>
2021-06-30 14:02:39 +0800

    test

--- test.php
+++ test.php
@@ -7,7 +7,7 @@ $message = array(
     'message'=>'Hello,Shane'
 );
 $ch = curl_init();
-curl_setopt($ch , CURLOPT_URL , "https://supervise-system.herokuapp.com/test.php");
+curl_setopt($ch , CURLOPT_URL , "https://notify-api.line.me/api/notify");
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $message);
